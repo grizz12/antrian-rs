1ce88eae8855e31cc554df84634b70268efd74ab
SalmanIrsyad <salman_070@smkassalaambandung.sch.id>
2022-12-12 16:02:38 +0700

    Revisian

--- resources/views/cetak/pesan.blade.php
+++ resources/views/cetak/pesan.blade.php
@@ -11,6 +11,12 @@
         text-align: left;
     }
 
+    .card-text{
+        float: left;
+        text-align: left;
+        
+    }
+
 </style>
 
     <div class="container">
@@ -18,11 +24,11 @@
             <div class="col-md-12">
                 @include('layouts/_flash')
                 <center>
-                    <div class="card" style="width: 50rem;height: 30rem;">
+                    <div class="card" style="width: 50rem;height: 32rem;">
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-md-auto">
-                                    <img src="{{asset('images')}}/PIKSI.png" alt="Piksi Ganesha" height="70" width="70">
+                                    <img src="{{asset('images')}}/PIKSI.png" alt="Piksi Ganesha" height="85" width="85">
                                 </div>
                                 <div class="col-md-auto">
                                     <h1><b>Tiket Reservasi</b></h1>
@@ -30,35 +36,45 @@
                                 </div>
                             </div>
                             <hr>
-                            <div>
-                                <div class="col">
-                                    <div class="text-body" style="float: left">
-                                            <h4>Nomor Antrian Anda : <br>
-                                                {{ $tiket->id }}   
-                                            </h4>
-                                            <br>
-                                            <h4>Nomor Rekam Medis Anda : <br>
-                                                {{ $tiket->DataPasien['no_rekam'] }}
-                                            </h4>   
-                                            <br>
-                                            <h4>Nama Dokter Anda : <br>
-                                                {{ $tiket->dokter }}
-                                            </h4>
-                                            <br>
-                                            </h2>
+                            <div class="container">
+                                <div class="row g-2">
+                                    <div class="col-6">
+                                        <div class="text-body">
+                                                <h4>Nomor Antrian Anda : <br>
+                                                    {{ $tiket->id }}   
+                                                </h4>
+                                                <br>
+                                                <h4>Nomor Rekam Medis Anda : <br>
+                                                    {{ $tiket->DataPasien['no_rekam'] }}
+                                                </h4>   
+                                                <br>
+                                                <h4>Nama Dokter Anda : <br>
+                                                    {{ $tiket->dokter }}
+                                                </h4>
+                                                <br>
+                                                
                                         </div>
-                                    </div> 
-                                </div>
-                                <div class="col">
-                                    <small style="float: right;">Tgl-Kunjungan : {{$tiket->tgl_kunjungan}}</small><br>
-                                    <small style="float: right;">Jam-Kunjungan : {{$tiket->waktu_kunjungan}}</small>
-                                </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <small style="float:right">Tgl Kunjungan : {{$tiket->tgl_kunjungan}}</small><br>
+                                        <small style="float:right">Jam Kunjungan : {{$tiket->waktu_kunjungan}}</small>
+                                    </div>
+                                </div>  
+                                <div class="card border-dark">
+                                    <div class="card-body">
+                                        <small class="card-text">
+                                            Catatan :<br>
+                                            Jika Anda Sudah Mencetak Tiket Ini,Datang Ke Resepsionis Piksi Hospital
+                                        </small>
+                                    </div>
+                                  </div>
                             </div> 
-                        </div>    
-                    <div class="mb-3" align="center">      
-                        <a href="/cetak_pdf/{{$tiket->id}}" class="btn btn-danger" target="_blank"><i class="fa fa-print"></i>&nbsp;Cetak PDF</a>
-                        <a href="{{ route('tiket.index') }}" class="btn btn-primary" type="submit">Kembali</a>
-                    </div>
+                        </div>  
+                    </div>    
+                <div class="mb-3">      
+                    <a href="/cetak_pdf/{{$tiket->id}}" class="btn btn-danger" target="_blank"><i class="fa fa-print"></i>&nbsp;Cetak PDF</a>
+                    <a href="{{ route('tiket.index') }}" class="btn btn-primary" type="submit">Kembali</a>
+                </div>
                 <br><br><br>
             </center>
             </div>
--- resources/views/cetak/pesan_pdf.blade.php
+++ resources/views/cetak/pesan_pdf.blade.php
@@ -7,35 +7,52 @@
     <title>Pesan Tiket</title>
  </head>
  <body>
-    <fieldset>
-        <div class="row">
-            <div class="col-md-auto">
-                <img src="{{asset('images')}}/PIKSI.png" alt="Piksi Ganesha" height="60" width="60">
-            </div>
-            <div class="col-md-auto">
-                <h1><b>Tiket Reservasi</b></h1>
-                <small>Pendaftaran Pasien Rawat Jalan Piksi Hospital</small>
-            </div>
-        </div>
-        <small style="float: right;">Tgl-Kunjungan : {{$tiket->tgl_kunjungan}}</small><br>
-        <small style="float: right;">Jam-Kunjungan : {{$tiket->waktu_kunjungan}}</small>
-            <div class="text-body" style="float: left">
-                <br>
-                    <h3>Nomor Antrian Anda : <br>
-                        {{ $tiket->id }}   
-                    </h3>
-                    <br>
-                    <h3>Nomor Rekam Medis Anda : <br>
-                        {{ $tiket->DataPasien['no_rekam'] }}
-                    </h3>   
-                    <br>
-                    <h3>Nama Dokter Anda : <br>
-                        {{ $tiket->dokter }}
-                    </h3>
-                    <br>
-                    </h2>
+    <div class="card" style="width: 50rem;height: 32rem;">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-auto">
+                    <p>asep</p>
                 </div>
-            </div>  
-    </fieldset>   
+                <div class="col-md-auto">
+                    <h1><b>Tiket Reservasi</b></h1>
+                    <small>Pendaftaran Pasien Rawat Jalan Piksi Hospital</small>
+                </div>
+            </div>
+            <hr>
+            <div class="container">
+                <div class="row g-2">
+                    <div class="col-6">
+                        <div class="text-body">
+                                <h4>Nomor Antrian Anda : <br>
+                                    {{ $tiket->id }}   
+                                </h4>
+                                <br>
+                                <h4>Nomor Rekam Medis Anda : <br>
+                                    {{ $tiket->DataPasien['no_rekam'] }}
+                                </h4>   
+                                <br>
+                                <h4>Nama Dokter Anda : <br>
+                                    {{ $tiket->dokter }}
+                                </h4>
+                                <br>
+                                
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <small style="float:right">Tgl Kunjungan : {{$tiket->tgl_kunjungan}}</small><br>
+                        <small style="float:right">Jam Kunjungan : {{$tiket->waktu_kunjungan}}</small>
+                    </div>
+                </div>  
+                <div class="card border-dark">
+                    <div class="card-body">
+                        <small class="card-text">
+                            Catatan :<br>
+                            Jika Anda Sudah Mencetak Tiket Ini,Datang Ke Resepsionis Piksi Hospital
+                        </small>
+                    </div>
+                  </div>
+            </div> 
+        </div>  
+    </div>
  </body>
  </html>
